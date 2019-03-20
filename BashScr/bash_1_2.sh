c6bc216f4c7f425d15b5808fd510b5320f67d518
justine05 <justinebiju05@gmail.com>
2019-03-20 21:09:45 +0530

    First BashScr Added

--- BashScr/bash_1_2.sh
+++ BashScr/bash_1_2.sh
@@ -1,6 +1,6 @@
 echo -e "\e[1mThe current OS is:\e[0m " `uname -o`
 echo
-echo -e "\e[1mThe OS version is:\e[0m " `uname -v`
+echo -e "\e[1mThe OS version is:\e[0m " `uname -a`
 echo
 echo -e "\e[1mThe available shells are as follows:\e[0m "
 cat /etc/shells/
@@ -12,7 +12,7 @@ echo -e "\e[1mThe memory information is as follows:\e[0m "
 cat /proc/meminfo
 echo
 echo -e "\e[1mThe hard disk information is as follows:\e[0m "
-free -h
+lsblk
 echo
 echo -e "\e[1mMounted file system information:\e[0m "
 df -Th
