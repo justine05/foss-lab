75826022ccad1fd03f91c5df7eef1c7c7a898445
justine05 <justinebiju05@gmail.com>
2019-03-26 01:35:50 +0530

    Added second BashScr

--- BashScr/bash_2_1.sh
+++ BashScr/bash_2_1.sh
@@ -1,19 +1,24 @@
-if [[ $# -ne 1 ]]
+if [[ $# -eq 0 ]]
 then
 	echo "No file specified as an argument"
 	exit
 fi
+for file in $*
+do
+	if [[ !(-a $file) ]]
+	then
+		echo "Enter a valid files as arguments"
+		exit
+	fi
+done
+for file in $*
+do
+	NAME=`basename $file`
+	PERMS=`ls -lah $file | awk -F " " '{print $1}'`
+	SIZE=`ls -lah $file | awk -F " " '{print $5}'`
+	DATE=`ls -lah $file | awk -F " " '{print $6 " " $7 " " $8 }'`
 
-if [[ !(-a $1) ]]
-then
-	echo "Enter a valid file as an argument"
-	exit
-fi
+	printf "The name of file is: $NAME\nThe last modified date is: $DATE"
+	printf "\nThe permissions are: $PERMS\nThe size is: $SIZE\n\n"
 
-PERMS=`ls -lah $1 | awk -F " " '{print $1}'`;
-SIZE=`ls -lah $1 | awk -F " " '{print $5}'`;
-NAME=`ls -lah $1 | awk -F " " '{print $9}'`;
-DATE=`ls -lah $1 | awk -F " " '{print $6 "\ " $7 "\ " $8 }'`;
-
-printf "The permissions are: $PERMS\nThe size is: $SIZE\n"
-printf "The name of file is: $NAME\nThe last modified date is: $DATE"
+done
